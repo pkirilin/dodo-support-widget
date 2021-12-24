b4f8de5521d3c308d42b79f5ce028dcb447b36ea
Pavel Kirilin <kirilin.pav@gmail.com>
2021-12-24 10:23:25 +0300

    Init frontend project

---
+++ frontend/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    minimize: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: 'index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+};
